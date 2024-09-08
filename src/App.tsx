a7a47e875cd673c9e414fd37f1a11e2f7b74f9ae
ManuLisek <miclisowiec@gmail.com>
2024-09-08 13:59:19 +0200

    Style the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import UsersComponent from './components/UsersComponent';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
 
 const App = () => {
   return (
-    <div className="App">
+    <StyledApp>
       <UsersComponent />
-    </div>
+    </StyledApp>
   );
 };
 
